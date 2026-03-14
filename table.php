5a721c4e4973151ef789e289c6d6b765f248b116
Yağmur Sadi <yagmuursadi@gmail.com>
2026-03-15 00:53:31 +0300

    Basit Table

---
+++ table.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <table width="400" border="1">
+  <tbody>
+    <tr>
+      <td>1 satır 1 sütun</td>
+      <td>1 satır 2 sütun</td>
+      <td>1 satır 3 sütun</td>
+    </tr>
+    <tr>
+      <td>2 satır 1 sütun</td>
+      <td>2 satır 2 sütun</td>
+      <td>2 satır 3 sütun</td>
+    </tr>
+    <tr>
+      <td>3 satır 1 sütun</td>
+      <td>3 satır 2 sütun</td>
+      <td>3 satır 3 sütun</td>
+    </tr>
+  </tbody>
+</table>
+</body>
+</html>
